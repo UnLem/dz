fd85c92a3d752af0ced69359bc0d9dc31f0ddc03
UnLem <dimnokin@mail.ru>
2020-09-02 15:18:33 +0600

    dz

---
+++ dzGit/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dzGit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string again;
+            do
+            {
+                Console.WriteLine("Введите число ");
+                int num = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Введите степень, в которую необходимо возвести число ");
+                int pow = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Результат "+Math.Pow(num, pow));
+                Console.WriteLine("Нажмите 0 для повтора ");
+                again = Console.ReadLine();
+            } while (again == "0");
+        }
+    }
+}
